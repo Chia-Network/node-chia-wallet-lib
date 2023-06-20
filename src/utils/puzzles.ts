01dfda008fc5d21c88faeb6f81b0084a92452379
Rigidity <me@rigidnetwork.com>
2023-06-19 22:19:16 -0400

    Add CAT and reorder

--- src/utils/puzzles.ts
+++ src/utils/puzzles.ts
@@ -10,21 +10,25 @@ const puzzle = (name: string) =>
         fs.readFileSync(path.join(walletPath, `${name}.clsp.hex`), 'utf-8')
     );
 
-export const DID = puzzle('did');
+export const STANDARD_TRANSACTION = puzzle('standard-transaction');
+export const HIDDEN_PUZZLE = puzzle('hidden-puzzle');
+export const HIDDEN_PUZZLE_HASH = HIDDEN_PUZZLE.hash();
+
 export const SINGLETON = puzzle('singleton');
 export const SINGLETON_LAUNCHER = puzzle('singleton-launcher');
-export const STANDARD_TRANSACTION = puzzle('standard-transaction');
-export const NFT_STATE_LAYER = puzzle('nft-state-layer');
-export const NFT_OWNERSHIP_LAYER = puzzle('nft-ownership-layer');
-export const NFT_TRANSFER_ROYALTIES = puzzle('nft-transfer-royalties');
-export const NFT_TRANSFER_PROGRAM = puzzle('nft-transfer-program');
-export const HIDDEN_PUZZLE = puzzle('hidden-puzzle');
-
 export const SINGLETON_HASH = SINGLETON.hash();
 export const SINGLETON_LAUNCHER_HASH = SINGLETON_LAUNCHER.hash();
-export const NFT_OWNERSHIP_LAYER_HASH = NFT_OWNERSHIP_LAYER.hash();
+
+export const CAT = puzzle('cat');
+export const DID = puzzle('did');
+
+export const NFT_STATE_LAYER = puzzle('nft-state-layer');
+export const NFT_OWNERSHIP_LAYER = puzzle('nft-ownership-layer');
 export const NFT_STATE_LAYER_HASH = NFT_STATE_LAYER.hash();
-export const HIDDEN_PUZZLE_HASH = HIDDEN_PUZZLE.hash();
+export const NFT_OWNERSHIP_LAYER_HASH = NFT_OWNERSHIP_LAYER.hash();
+
+export const NFT_TRANSFER_ROYALTIES = puzzle('nft-transfer-royalties');
+export const NFT_TRANSFER_PROGRAM = puzzle('nft-transfer-program');
 
 export function createSingletonStruct(
     singletonModHash: Uint8Array,
