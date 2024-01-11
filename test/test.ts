172cd5a19366f7e8faa76878d6361b9d9adfea8e
Don Kackman <dkackman@gmail.com>
2024-01-10 21:59:12 -0600

    passing secret key tests

--- test/test.ts
+++ test/test.ts
@@ -3,7 +3,9 @@ import { assert } from 'chai';
 import { describe } from 'mocha';
 
 import { PrivateKey, AugSchemeMPL } from 'chia-bls';
-import { derivePublicKeyPath, derivePublicKey, calculateSyntheticPublicKey } from '../src/utils/keys';
+import { derivePublicKeyPath, derivePrivateKeyPath, calculateSyntheticPublicKey, calculateSyntheticPrivateKey } from '../src/utils/keys';
+
+const DEFAULT_HIDDEN_PUZZLE_HASH: Uint8Array = Buffer.from("711d6c4e32c92e53179b199484cf8c897542bc57f2b22582799f9d657eec4699", 'hex');
 
 describe('Synthetic public keys', () => {
     const hexKeys = [
@@ -25,7 +27,6 @@ describe('Synthetic public keys', () => {
         "b3b4ceea11bbc6fafb5800caa593385644a3262245357e5013be5c1cf622bf7cb0b667e586269c346459c3b5faf0eaef"
     ];
 
-    const DEFAULT_HIDDEN_PUZZLE_HASH: Uint8Array = Buffer.from("711d6c4e32c92e53179b199484cf8c897542bc57f2b22582799f9d657eec4699", 'hex');
     const sk = PrivateKey.fromHex("6bb19282e27bc6e7e397fb19efc2627a412410fdfd13bf14f4ce5bfdce084c71");
     const pk = sk.getG1();
     const intermediate = derivePublicKeyPath(pk, [12381, 8444, 2]);
@@ -36,6 +37,41 @@ describe('Synthetic public keys', () => {
             const syntheticKey = calculateSyntheticPublicKey(key, DEFAULT_HIDDEN_PUZZLE_HASH);
             const syntheticKeyHex = syntheticKey.toHex();
 
+            assert.equal(syntheticKeyHex, hex);
+        });
+    });
+});
+
+
+describe('Synthetic secret keys', () => {
+
+    const hexKeys = [
+        "64c91fe4534fc21c36096be012e0e14de484180a1a510783367bcd5ccecaad0c",
+        "13a0f95de0dd347c769ee79e9828a698bfe53429233375e891f05b4e0eaa8219",
+        "4399fcc4435e014f24fa31f8acf419367e0fbac70c9d6df53a7cc31623a10eac",
+        "5ef7d10f546d45ae919277568d2142f9e907933d4393111c54b13425c20abce1",
+        "0c39c6c5d70ee05cbd3da44ae918af21469f235dc0bea0116566a4379d33f1f5",
+        "39fbf8ed5b0b7071ba155d7a0180af13ea88aec08d74afaa86ea11ad61cb06ec",
+        "31c03611b14be9c811beb435162e2c6e773efc77a4a5a5b2aca87e27687f6a12",
+        "222b62feb12390110122c63c176c2216cf85ad17bdbfd6240663eeeeab47abc0",
+        "18012a00d6ab3aec680960cc21beec41b78c731be1720f0c30f09e32892e918f",
+        "31e8f4a82182f072cb9035acdb45ae3e4c5d631b4538dd89afa71294792a774f",
+        "196f72aaa1403dc0e3334a3b7a1910fbe61dfabd3aba178ed7e1ba7c80dccce2",
+        "27bf40f3a6fa7b2b47f48402e423ee2c5a441e6ba716fc6fef02c3dd5285df20",
+        "0bccd277dbeb9742e816b0696b298277985ec7008ba61a585af63b4a4b6d7108",
+        "3d77df05a9bbda18cc23de6ac1b9744f0a07622df925ae253b3418c41709dcc8",
+        "465a7d9a106522d4111e5f35ca5ca4fb6889b884f15eba024e3826b8191cf784",
+        "68fe97fadd389f4051b6bd0c320f17d08ba7b4951b33d525b1f23d38226e5d0a",
+    ];
+
+    const sk = PrivateKey.fromHex("6bb19282e27bc6e7e397fb19efc2627a412410fdfd13bf14f4ce5bfdce084c71");
+    const intermediate = derivePrivateKeyPath(sk, [12381, 8444, 2], false);
+
+    hexKeys.forEach((hex, index) => {
+        it(`Index ${index}`, () => {
+            const key = AugSchemeMPL.deriveChildSkUnhardened(intermediate, index);
+            const syntheticKey = calculateSyntheticPrivateKey(key, DEFAULT_HIDDEN_PUZZLE_HASH);
+            const syntheticKeyHex = syntheticKey.toHex();
             assert.equal(syntheticKeyHex, hex);
         });
     });
